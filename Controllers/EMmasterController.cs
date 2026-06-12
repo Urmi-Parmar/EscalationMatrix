612a15bfc13fdc27b34c19ea28fa550248ad3cd8
User <User@DESKTOP-EL5HNN6>
2026-06-12 15:28:20 +0530

    Add project files.

---
+++ Controllers/EMmasterController.cs
@@ -0,0 +1,43 @@
+﻿using EscalationMatrix.DBClass;
+using EscalationMatrix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EscalationMatrix.Controllers
+{
+    public class EMmasterController : Controller
+    {
+        EMatrixEntities DbContext = new EMatrixEntities();
+        // GET: EMmaster
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult EMList()
+        {
+            var q1 = DbContext.EM_Header
+                     .Select(x => new EMatrixViewModel
+                     {
+                         HeaderID = x.EMHeader,
+                         ComplaintType = x.ComplaintType,
+                         ScopeofWork = x.ScopeofWork,
+                         Addedby = x.Addedby,
+                         AddedOn = x.AddedOn,
+                         Updateby = x.Updateby,
+                         UpdateOn = x.UpdateOn
+                     })
+                     .ToList();
+
+            return View(q1);
+        }
+        public ActionResult CreateEM()
+        {
+            var q1 = DbContext.
+            return View();
+        }
+    }
+}
---
+++ Models/EMatrixViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EscalationMatrix.Models
+{
+	public class EMatrixViewModel
+	{
+		public int HeaderID { get; set; }
+		public string ComplaintType { get; set; }
+		public string ScopeofWork { get; set; }
+		public int? Addedby { get; set;}
+		public DateTime? AddedOn { get; set; }
+		public int? Updateby { get; set; }
+		public DateTime? UpdateOn { get; set; }
+    }
+    public class EMatrixHistoryViewModel
+    {
+        public int HeaderID { get; set; }
+        public int HID { get; set; }
+        public string Window { get; set; }
+        public string NameOfAssociate { get; set; }
+        public string ContactDetails { get; set; }        
+        public int? Addedby { get; set; }
+        public DateTime AddedOn { get; set; }
+        public int? Updateby { get; set; }
+        public DateTime UpdateOn { get; set; }
+    }
+    public class DepartmentMaster
+    {
+        public int Id { get; set; }
+        public int? DId { get; set; }
+        public string Department { get; set; }
+    }
+}
