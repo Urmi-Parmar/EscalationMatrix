04432028f6c61ce42b3d2fc0bee101cfb3993bd2
Urmi <urmi@ramansheeinfotech.com>
2026-06-13 12:32:43 +0530

    add layer

---
+++ IService/IEscalationMatrixService.cs
@@ -0,0 +1,13 @@
+﻿using EscalationMatrix.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace EscalationMatrix.IService
+{
+	public class IEscalationMatrixService
+	{
+		Task<List<EMatrixViewModel>> GetData();
+
+    }
+}
